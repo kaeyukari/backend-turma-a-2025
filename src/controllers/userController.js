005eb035e1018cfb6801a94ef6f1add9e602ea87
kaeyukari <karenyukariyt19@gmail.com>
2025-06-15 20:42:00 +0000

    aula #7

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,16 @@
-export const getUsers = (req, res) => {
+import { findAll } from '../models/userModel.js';
+
+export const getUsers = async (req, res) => {
     try {
-        // buscar os usuários do banco de dados
-        res.status(200).json({ menssage: "Get Users"});
+        // buscar os usuários do banco de dados 
+
+        const users = await findAll();
+        
+        res.status(200).json(users);
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: 'Internal server error - Controller' });
         
     }
 }
+
