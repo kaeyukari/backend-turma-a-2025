5151d759c754f70df9dd2bca4cf56553072a70e4
kaeyukari <karenyukariyt19@gmail.com>
2025-06-16 00:00:41 +0000

    aula #9

--- src/routes/payment.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-import paymentController from '../controlles/payment.controller.js';
-
-router.post("/payment", paymentController.createPayment);
-router.patch("/payment/:id", paymentController.updatePayment);
-
-export default router;
